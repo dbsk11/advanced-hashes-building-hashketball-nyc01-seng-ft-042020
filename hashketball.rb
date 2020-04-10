3b8024e94062a821d061408869700f020f6da8b4
Jessica K <jessikim807@gmail.com>
2020-04-10 21:29:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 def game_hash
   {
-    :home => {
-      :team_name => "Brooklyn Nets",
-      :colors => ["Black", "White"],
-      :players => [
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
         {
           :player_name => "Alan Anderson",
           :number => 0,
