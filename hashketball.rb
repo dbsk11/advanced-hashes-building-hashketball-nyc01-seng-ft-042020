cf4b375b80fa41c3a2e7369a13ff6299cb5ade2f
Jessica K <jessikim807@gmail.com>
2020-04-10 21:45:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -65,7 +65,7 @@ def game_hash
           steals: 2,
           blocks: 7,
           slam_dunks: 2},
-        { player_name: "Bismarck Biyombo",
+        { player_name: "Bismack Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
